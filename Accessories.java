c8234723c218aa6983ae9302cb09dc60190d6d7e
Sailesh-126 <saileshnichenametla@gmail.com>
2024-11-14 18:33:38 +0530

    Update Accessories.java

--- Accessories.java
+++ Accessories.java
@@ -7,9 +7,13 @@ import java.awt.event.MouseMotionListener;
 public class Accessories extends JLabel implements MouseListener, MouseMotionListener, Crafted_Spaces
 {
     Point prevpt;
-    int length, breadth;
+    Point finalpt;
+    Point initialpt;
+    int length;
+    int breadth;
     Accessories()
     {
+        accessoryHistory.add(this);
         this.addMouseListener(this);
         this.addMouseMotionListener(this);
         this.setOpaque(false);
@@ -23,11 +27,27 @@ public class Accessories extends JLabel implements MouseListener, MouseMotionLis
     @Override
     public void mousePressed(MouseEvent e) {
         prevpt = e.getPoint();
+        initialpt = this.getLocation();
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
+        for(Accessories a: accessoryHistory) {
+            if(this.getX()>a.getX() && this.getX()< a.getX()+a.breadth){
+                if((this.getY()>a.getY() && this.getY()<a.getY()+a.length) || (this.getY()+this.length>a.getY() && this.getY()+this.length<a.getY()+a.length)){
+                    JOptionPane.showMessageDialog(null,"Overlap", "ERROR", JOptionPane.WARNING_MESSAGE);
+                    this.setBounds(initialpt.x, initialpt.y, this.breadth, this.length);
+                }
+            }
+            if(this.getX()+this.breadth>a.getX() && this.getX()+this.breadth < a.getX()+a.breadth){
+                if((this.getY()>a.getY() && this.getY()<a.getY()+a.length) || (this.getY()+this.length>a.getY() && this.getY()+this.length<a.getY()+a.length)){
+                    JOptionPane.showMessageDialog(null,"Overlap", "ERROR", JOptionPane.WARNING_MESSAGE);
+                    this.setBounds(initialpt.x, initialpt.y, this.breadth, this.length);
+                }
+            }
+        }
 
+        finalpt = this.getLocation();
     }
 
     @Override
@@ -66,6 +86,8 @@ class sbed extends furniture
 {
     sbed()
     {
+        length = icon1.getIconHeight();
+        breadth = icon1.getIconWidth();
         this.setBounds(0,0,150,100);
         this.setIcon(icon1);
     }
@@ -75,6 +97,8 @@ class lbed extends furniture
 {
     lbed()
     {
+        length = icon2.getIconHeight();
+        breadth = icon2.getIconWidth();
         this.setBounds(0,0,150,100);
         this.setIcon(icon2);
     }
@@ -83,6 +107,8 @@ class shelf1 extends furniture
 {
     shelf1()
     {
+        length = icon3.getIconHeight();
+        breadth = icon3.getIconWidth();
         this.setBounds(0,0,150,100);
         this.setIcon(icon3);
     }
@@ -91,6 +117,8 @@ class shelf2 extends furniture
 {
     shelf2()
     {
+        length = icon4.getIconHeight();
+        breadth = icon4.getIconWidth();
 
         this.setBounds(0,0,150,100);
         this.setIcon(icon4);
@@ -100,6 +128,8 @@ class sofa1 extends furniture
 {
     sofa1()
     {
+        length = icon5.getIconHeight();
+        breadth = icon5.getIconWidth();
         this.setBounds(0,0,150,100);
         this.setIcon(icon5);
     }
@@ -108,6 +138,8 @@ class sofa2 extends furniture
 {
     sofa2()
     {
+        length = icon6.getIconHeight();
+        breadth = icon6.getIconWidth();
         this.setBounds(0,0,150,100);
         this.setIcon(icon6);
     }
@@ -116,6 +148,8 @@ class sofa3 extends furniture
 {
     sofa3()
     {
+        length = icon7.getIconHeight();
+        breadth = icon7.getIconWidth();
         this.setBounds(0,0,150,100);
         this.setIcon(icon7);
     }
@@ -124,6 +158,8 @@ class table extends furniture
 {
     table()
     {
+        length = icon8.getIconHeight();
+        breadth = icon8.getIconWidth();
         this.setBounds(0,0,150,100);
         this.setIcon(icon8);
     }
@@ -132,6 +168,8 @@ class desk extends furniture
 {
     desk()
     {
+        length = icon9.getIconHeight();
+        breadth = icon9.getIconWidth();
         this.setBounds(0,0,150,100);
         this.setIcon(icon9);
     }
@@ -140,6 +178,8 @@ class bath extends furniture
 {
     bath()
     {
+        length = icon10.getIconHeight();
+        breadth = icon10.getIconWidth();
         this.setBounds(0,0,150,100);
         this.setIcon(icon10);
     }
@@ -148,6 +188,8 @@ class urinal extends furniture
 {
     urinal()
     {
+        length = icon11.getIconHeight();
+        breadth = icon11.getIconWidth();
         this.setBounds(0,0,150,100);
         this.setIcon(icon11);
     }
@@ -156,6 +198,8 @@ class sink extends furniture
 {
     sink()
     {
+        length = icon12.getIconHeight();
+        breadth = icon12.getIconWidth();
         this.setBounds(0,0,150,100);
         this.setIcon(icon12);
     }
