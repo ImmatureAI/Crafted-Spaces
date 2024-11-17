795b74168ee524635ade7c38a1274469e5399a85
Sailesh-126 <saileshnichenametla@gmail.com>
2024-11-17 10:09:16 +0530

    Update Details.java

--- Details.java
+++ Details.java
@@ -5,11 +5,14 @@ public class Details implements Serializable {
     //This class has the info about the room positions and furniture positions
 
     ArrayList<Room> rooms;
+    ArrayList<Accessories> accessories;
+    
     int width;
     int height;
 
-    Details(ArrayList<Room> rooms, int spacewidth, int spaceheight){
+    Details(ArrayList<Room> rooms, ArrayList <Accessories> accessories,int spacewidth, int spaceheight){
         this.rooms = rooms;
+        this.accessories = accessories;
         this.width = spacewidth;
         this.height = spaceheight;
     }
