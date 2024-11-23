ff285fa18d073e07479ecc04088e1cab52296720
Sailesh-126 <saileshnichenametla@gmail.com>
2024-11-23 14:21:18 +0530

    Update DoorPanel.java

--- DoorPanel.java
+++ DoorPanel.java
@@ -1,40 +1,45 @@
+import javax.print.attribute.standard.DateTimeAtCompleted;
 import javax.swing.*;
 import java.awt.*;
 
 public class DoorPanel extends JPanel{
     int doorLength;
-    Room room;
+    String pos;
 
-    String[] positions = {"North", "East", "South", "West"};
-    JComboBox<String> position = new JComboBox<>(positions);
 
-    DoorPanel(Room r,int doorLength){
+
+    DoorPanel(String pos, int doorLength){
         this.doorLength = doorLength;
-        this.room = r;
+        this.pos = pos;
     }
 
-    JPanel doorPanel(String pos){
+    JPanel doorPanel(Room room, String pos){
+        JPanel panel = new JPanel();
         switch(pos){
             case "North":
-                this.setBackground(Color.white);
-                this.setBounds(room.width/2 - doorLength/2,0,doorLength,5);
-                this.setOpaque(true);
-                this.setVisible(true);
-                return this;
+                panel.setBackground(Color.white);
+                panel.setBounds(room.width/2 - doorLength/2,0,doorLength,5);
+                //panel.setOpaque(true);
+                panel.setVisible(true);
+                //finalpt = this.getLocation();
+                return panel;
             case "East":
                 this.setBackground(Color.white);
                 this.setBounds((room.width - 5),(room.height/2 - doorLength /2), 5, doorLength);
                 this.setOpaque(true);
+                //finalpt = this.getLocation();
                 return this;
             case "South":
                 this.setBackground(Color.white);
                 this.setBounds((room.width/2 - doorLength /2), (room.height - 5), doorLength, 5);
                 this.setOpaque(true);
+                //finalpt = this.getLocation();
                 return this;
             case "West":
                 this.setBackground(Color.white);
                 this.setBounds(0,(room.height/2 - doorLength /2), 5, doorLength);
                 this.setOpaque(true);
+                //finalpt = this.getLocation();
                 return this;
             default:
                 return null;
