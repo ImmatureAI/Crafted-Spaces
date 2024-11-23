044fa5ae212276e0e268ef5f9f158006682dbb24
Sailesh-126 <saileshnichenametla@gmail.com>
2024-11-23 14:19:35 +0530

    Update Fixtures.java

--- Fixtures.java
+++ Fixtures.java
@@ -5,17 +5,21 @@ public class Fixtures extends Accessories{
     @Override
     public void mouseReleased(MouseEvent e) {
         for (Room r : history) {
-            boolean a = ((this.getX() <= r.getX() + 5) && (this.getX() >= r.getX() - 5));
-            boolean b = ((this.getY() <= r.getY() + 5) && (this.getY() >= r.getY() - 5));
-            boolean c = ((this.getX() + this.breadth <= r.getX() + r.width + 5) && (this.getX() + this.breadth >= r.getX() + r.width - 5));
-            boolean d = ((this.getY() + this.length <= r.getY() + r.height + 5) && (this.getY() + this.length >= r.getY() + r.height - 5));
-            boolean onWall = (a || b || c || d);
+            if (((this.getX() >= r.getX()) && (this.getX() + this.breadth <= r.getX() + r.width)) && ((this.getY() >= r.getY()) && (this.getY() + this.length <= r.getY() + r.height))) {
+                boolean a = ((this.getX() <= r.getX() + 5) && (this.getX() >= r.getX() - 5));
+                boolean b = ((this.getY() <= r.getY() + 5) && (this.getY() >= r.getY() - 5));
+                boolean c = ((this.getX() + this.breadth <= r.getX() + r.width + 5) && (this.getX() + this.breadth >= r.getX() + r.width - 5));
+                boolean d = ((this.getY() + this.length <= r.getY() + r.height + 5) && (this.getY() + this.length >= r.getY() + r.height - 5));
+                boolean onWall = (a || b || c || d);
 
-            if (!onWall) {
-                JOptionPane.showMessageDialog(null, "Fixtures must be placed on the walls", "ERROR", JOptionPane.WARNING_MESSAGE);
-                this.setBounds(initialpt.x, initialpt.y, this.breadth, this.length);
+                if (!onWall) {
+                    JOptionPane.showMessageDialog(null, "Fixtures must be placed on the walls", "ERROR", JOptionPane.WARNING_MESSAGE);
+                    this.setBounds(initialpt.x, initialpt.y, this.breadth, this.length);
+                }
             }
         }
+
+
     }
 }
 
